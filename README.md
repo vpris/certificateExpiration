418f1ff58b1f94ca5132c9cf63c4cdde9670960f
Vladimir Prisyazhnikov <vprisyazhnikov@utrace.ru>
2019-10-21 15:57:54 +0600

    Adding Readme and version 0.1.0

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # sslCertificateExpiration
+Author: <b>Vladimir Prisyazhnikov (Chuck Tornton)</b><br>
+Current version: <b>0.1.0</b>
+
+This script allows you to get the number of days before the expiration of the certificate or the number of days in percent.
+
+#### Where applicable:
+ - You can receive information in the console and be satisfied with it.
+ - can be connected to zabbix. The script will return a number that can already be operated on.
+For example, configure monitoring to warn that after a certain time the certificate will expire and it is time to start acting.
+ - in any other situations in which your imagination wants to use such data.
+
+
+
+#### For Alpine Linux:
+
+Please install:
+
+- Select your timezone.
+- apk add --update && apk add coreutils
+
+#### For Ubuntu:
+
+- Select your timezone.
+- Working works out of the box.
+
+### How to use:
+
+- Make the file executable:<br>
+  chmod +x cert.sh
+- Run the command:<br>
+ <b>./cert.sh google.com </b>  or  <b> ./cert.sh google.com day </b><br>
+to get the number of days left
+
+- Run the command: <br>
+ <b>./cert.sh google.com percent</b> <br>
+to get the amount of remaining interest, where 100% =
+full validity of the certificate.
+
+#### If you want to use the script in Zabbix:
+
+- Copy in <b>cert.sh</b> to the <b>/etc/zabbix/externalscripts/</b>
+- Make the copied file executable: <b>chmod +x cert.sh</b>
+- Make Zabbix the owner of the file: <b> chown zabbix cert.sh </b>
--- cert.sh
+++ cert.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
 
-#Declare the variable SERVER.
+#Author: Vladimir Prisyazhnikov (Chuck Tornton)
 
 SERVER=$1
+UNIT=$2
 
-#We get the expiration date of the ssl certificate. We translate it into a timestamp, and then we bring it to the format YYYY-MM-DD (e.g. 2019-08-10).
+dateNotBefore=$(echo | openssl s_client -connect $SERVER:443 -servername $SERVER -tlsextdebug 2>/dev/null | openssl x509 -noout -dates 2>/dev/null | grep notBefore | cut -d'=' -f2)
+dateNotBeforeStamp=$(date +"%s" -d "$dateNotBefore")
+dateNotBeforeClear=$(date +"%Y-%m-%d" -d "@$dateNotBeforeStamp")
 
 dateNotAfter=$(echo | openssl s_client -connect $SERVER:443 -servername $SERVER -tlsextdebug 2>/dev/null | openssl x509 -noout -dates 2>/dev/null | grep notAfter | cut -d'=' -f2)
 dateNotAfterStamp=$(date +"%s" -d "$dateNotAfter")
 dateNotAfterClear=$(date +"%Y-%m-%d" -d "@$dateNotAfterStamp")
 
-#We get the current date in timestamp format, then we convert to the format YYYY-MM-DD (e.g. 2019-10-17).
-
 currentDateStamp=$(date +"%s")
 currentDateClear=$(date +"%Y-%m-%d" -d "@$currentDateStamp")
 
-#Count the number of days left. Subtract the current date from the expiration date of the certificate and divide by 86400 (the number of seconds in 24 hours).
-#P.S. You can take away the moment with bringing dates to a beautiful format and leave them at the time of bringing them to timestamp.
-#Substitute the results as variables in brackets and get the same result. But I like it more.
+results=$(( ($(date -d $dateNotAfterClear +%s) - $(date -d $dateNotBeforeClear +%s)) / 86400 ))
 
-result=$(( ($(date -d $dateNotAfterClear +%s) - $(date -d $currentDateClear +%s)) / 86400 ))
-echo -e "\e[42m $result \e[0m"
+day=$(( ($(date -d $dateNotAfterClear +%s) - $(date -d $currentDateClear +%s)) / 86400 ))
 
-# Where applicable:
-# - You can receive information in the console and be satisfied with it.
-# - can be connected to zabbix. The script will return a number that can already be operated on.
-#   For example, configure monitoring to warn that after a certain time the certificate will expire and it is time to start acting.
-# - - in any other situations in which your imagination wants to use such data.
+if [[ "$UNIT" == "percent" ]]; then
+    let "percent = ($day * 100) / $results"
+    echo $percent
+else
+    echo $day
+fi
